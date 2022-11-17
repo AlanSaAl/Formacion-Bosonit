019bab707af5c83db7110e1b6510cb0059d71eaf
alan.sandoval <sandoval.almaraz@gmail.com>
2022-11-17 11:33:16 -0600

    Primer ejercicio terminado y se añade el tercer ejercicio.

--- block1-process-file-and-streams/src/main/java/org/example/Fichero.java
+++ block1-process-file-and-streams/src/main/java/org/example/Fichero.java
@@ -6,49 +6,71 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.testng.AssertJUnit.assertEquals;
 
-// Clase para leer archivos
 public class Fichero {
+
+    // Lee un archivo una línea a la vez y devuelve una lista con los
     @Test
-    public List<String> leer_fichero(String ruta) throws IOException, InvalidLineFormatException {
-        System.out.println("--- Dentro de la funcion leer_fichero ---");
-
+    public List<String> leer_fichero(String ruta) throws IOException {
         Path path = Paths.get(ruta);
-
-        String pattern = "[abc]-[abc]-[0-9]{2}";
-        String expected_value = "Hello, world!";
-
         List<String> read = Files.readAllLines(path);
-        //assertEquals(expected_value, read);
+        List<Persona> personas = new ArrayList<>();
+        int contador = 0;
 
-        /*read.forEach(l -> {
-            if(l.length() > 15)
-                try {
-                    throw new InvalidLineFormatException("Línea no valida");
-                } catch (InvalidLineFormatException e) {
-                    throw new RuntimeException(e);
-                }
-        });*/
-
-        for(String reg:read){
-            if(reg.matches(pattern)){
-                try {
-                    throw new InvalidLineFormatException("Formato de línea no valido");
-                } catch (InvalidLineFormatException e) {
-                    throw new RuntimeException(e);
-                }
+        for (String lin : read) {
+            try {
+                contador++;
+                personas.add(validar_formato(lin, contador));
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
 
+        // Filtrado de personas
+        personas.forEach(p -> System.out.println(p));
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        personas.stream()
+                .filter(p -> p.getEdad().orElse(0) < 25).forEach(p -> System.out.println(p));
+
+        personas.stream()
+                .map(p ->
+                {
+                    return p.getNombre();
+                }).forEach(p -> System.out.println(p));
+
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        var persA = personas.stream().filter(p -> !p.getNombre().startsWith("A")).
+                collect(Collectors.toList());
+
+        persA.forEach(p -> System.out.println(p));
+        //personas.stream().filter( p -> ! p.getNombre().startsWith("A")).forEach( p -> System.out.println(p));
         return read;
     }
 
-    /*public List<String> filtrar_personas(List<String> listaDePersonas){
+    // Valida que el formato de una línea sea correcto
+    public Persona validar_formato(String linea, int nLinea) throws InvalidLineFormatException {
+        //  long contador = linea.chars().filter(c -> c == ':').count();
+        String[] lineaDividida = linea.split(":");
+        long contador = lineaDividida.length;
 
+//        if(contador <= 2){
+//            throw new InvalidLineFormatException("Faltan delimitadores de campo en en linea: "+nLinea+ " Linea: "+linea);
+//        } else if(lineaDividida[0].trim().equals("")){
+//            throw new InvalidLineFormatException("El campo nombre es obligatorio");
+//        }
 
-        //return listaFiltrada
-    }*/
+        Integer edad = null;
+        if (contador > 2)
+            edad = Integer.parseInt(lineaDividida[2]);
+        return new Persona(lineaDividida[0],
+                edad, lineaDividida[1]);
+    }
 }
---
+++ block1-process-file-and-streams/src/main/java/org/example/Persona.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Optional;
+
+public class Persona {
+    private String nombre;
+    private Optional<Integer> edad;
+    private String ciudad;
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", ciudad='" + ciudad + '\'' +
+                '}';
+    }
+
+    public Persona(String nombre, Integer edad, String ciudad) {
+        this.nombre = nombre;
+        this.edad = Optional.of(edad);
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Optional<Integer> getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = Optional.of(edad);
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
