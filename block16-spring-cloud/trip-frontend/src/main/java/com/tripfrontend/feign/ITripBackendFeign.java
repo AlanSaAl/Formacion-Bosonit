b73ec870345085ba1229ef6d15a52b9f042d0c91
alan.sandoval <sandoval.almaraz@gmail.com>
2023-01-27 14:55:11 -0600

    Block16-spring-cloud dockerizado.

--- block16-spring-cloud/trip-frontend/src/main/java/com/tripfrontend/feign/ITripBackendFeign.java
+++ block16-spring-cloud/trip-frontend/src/main/java/com/tripfrontend/feign/ITripBackendFeign.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", name = "tripBackend")
+@FeignClient(url = "http://tripbackend:8080", name = "tripBackend")
 public interface ITripBackendFeign {
     @GetMapping("cliente/obtener/{idCliente}")
     ClienteOutputDto getClienteById(@PathVariable int idCliente);
-    // get viaje por id
+
     @GetMapping("viaje/obtener/{idViaje}")
     ViajeOutputDto getViajeById(@PathVariable int idViaje);
 }
